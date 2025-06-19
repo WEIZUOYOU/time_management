a4663cad9df26c3b873bdfe4d08fc583bd89d1f3
wzy <W3014339588@163.com>
2025-06-19 22:05:55 +0800

    可运行

---
+++ build/CMakeFiles/time_management.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/time_management.dir/src/achievement.c.obj"
+  "CMakeFiles/time_management.dir/src/achievement.c.obj.d"
+  "CMakeFiles/time_management.dir/src/data.c.obj"
+  "CMakeFiles/time_management.dir/src/data.c.obj.d"
+  "CMakeFiles/time_management.dir/src/main.c.obj"
+  "CMakeFiles/time_management.dir/src/main.c.obj.d"
+  "CMakeFiles/time_management.dir/src/trash.c.obj"
+  "CMakeFiles/time_management.dir/src/trash.c.obj.d"
+  "libtime_management.dll.a"
+  "time_management.exe"
+  "time_management.exe.manifest"
+  "time_management.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/time_management.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/time_management.dir/src/data.c.obj.d
@@ -0,0 +1,18 @@
+CMakeFiles/time_management.dir/src/data.c.obj: \
+ D:\TASK\CLASS\大二\综合实训一\time_management\src\data.c \
+ D:/TASK/CLASS/大二/综合实训一/time_management/include/data.h \
+ D:/msys64/mingw64/include/raylib.h \
+ D:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.1.0/include/stdarg.h \
+ D:/msys64/mingw64/include/stdarg.h \
+ D:/msys64/mingw64/include/_mingw_stdarg.h \
+ D:/msys64/mingw64/include/vadefs.h D:/msys64/mingw64/include/_mingw.h \
+ D:/msys64/mingw64/include/_mingw_mac.h \
+ D:/msys64/mingw64/include/_mingw_secapi.h \
+ D:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ D:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.1.0/include/stdbool.h \
+ D:/msys64/mingw64/include/stdio.h \
+ D:/msys64/mingw64/include/corecrt_stdio_config.h \
+ D:/msys64/mingw64/include/corecrt.h \
+ D:/msys64/mingw64/include/_mingw_off_t.h \
+ D:/msys64/mingw64/include/swprintf.inl \
+ D:/msys64/mingw64/include/sec_api/stdio_s.h
